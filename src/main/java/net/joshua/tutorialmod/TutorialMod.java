5448b09752c5b6e3c22d9a7492085e67548a5945
joshuaG <joshua.guzman.7@my.csun.edu>
2025-12-26 00:50:36 -0800

    added custom blocks

--- src/main/java/net/joshua/tutorialmod/TutorialMod.java
+++ src/main/java/net/joshua/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.joshua.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.joshua.tutorialmod.block.ModBlocks;
 import net.joshua.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
